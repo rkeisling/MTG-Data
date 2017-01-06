e22c7fdf7f9a38d154f70168029dab1df1945eec
Ricky Keisling <rkeisling@basecampcodingacademy.org>
2017-01-06 14:55:01 -0600

    Removed old pony files and merged conflicted files

--- mtg_pony/main.py
+++
@@ -1,8 +0,0 @@
-import scrape_deckbox
-import get_prices
-import put_in_db
-
-if __name__ == '__main__':
-    scrape_deckbox.main()
-    get_prices.main()
-    put_in_db.main()
--- transfer_db.py
+++
@@ -1,6 +0,0 @@
-from pony.orm import db_session
-import mtg_pony.db as ponydb
-import mtg_django as djangodb
-
-
-def get_from_pony:
